2ac1d940e72366b69bedb84e74e9af14c65f8d47
Sergey Andreev <DragorWW@gmail.com>
2024-11-24 01:58:57 +0700

    CSS: seporate main styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,13 @@
-import '../css/main.css'
-
-import './matrix-animation.js'
-import './terminal-animation.js'
-import './console-easter-egg.js'
-import './theme-switcher.js'
+import './matrix-animation.js';
+import './terminal-animation.js';
+import './console-easter-egg.js';
+import './theme-switcher.js';
 
 // Инициализация
 document.addEventListener('DOMContentLoaded', () => {
     initMatrixAnimation('matrix', 1000);
     initTerminalAnimation();
-}) 
+});
 
 // Добавьте в начало файла
 if (import.meta.hot) {
